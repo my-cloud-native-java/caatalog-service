0bd59fbb166880378319d5f6fa62d1996536af26
Sohail Yunus <sohailyunus.shaik@beehyv.com>
2024-04-08 21:40:40 +0530

    Add configuration to load test data in local env.

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,11 +2,14 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
   public static void main(String[] args) {
+    System.out.println(System.getenv("polar.testdata.enabled"));
     SpringApplication.run(CatalogServiceApplication.class, args);
   }
 }
--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,13 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+  @Autowired private final PolarProperties polarProperties;
+
+  public HomeController(PolarProperties polarProperties) {
+    this.polarProperties = polarProperties;
+  }
+
   @GetMapping("/")
   public String getGreeting() {
-    return "welcome to the book catalog!";
+    return polarProperties.getGreeting();
   }
 }
